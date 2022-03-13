8f968db9c17b16ec97091c09c9e1ff093851b8ae
kekecoder <abimbola.felix@gmail.com>
2022-03-13 17:01:02 +0100

    updated database to heroku credential

--- Class/dbconfig.php
+++ Class/dbconfig.php
@@ -1,21 +1,29 @@
 <?php
-try{
-  $dbhost = "localhost";
-  $dbuser = "root";
-  $dbname = "noteapp";
-  $dbpass = "jerusalem1991";
-  $conn = new PDO("mysql:host=$dbhost; port=3306; dbname=$dbname;", $dbuser, $dbpass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- // print "connected successfully";
- $sql = "CREATE TABLE notes(
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NULL,
-  created_at DATETIME
-  ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci";
-  $conn->exec($sql);
-  print "Notes table created successfully"; 
-}catch(Error $e){
-  //print 'Something went wrong ' . $e->getMessage();
-  print 'Something went wrong, please try again later, thanks';
-}
+try 
+{
+        $url = getenv('JAWSDB_MARIA_URL');
+        $dbparts = parse_url($url);
+  
+        $hostname = $dbparts['host'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $database = ltrim($dbparts['path'],'/');
+        $pdo = new PDO("mysql:host=$hostname; port=3306; dbname=$database; ", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+      //print "connected successfully";
+      $sql = "CREATE TABLE notes(
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        created_at DATETIME
+        ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci";
+        //$sql = "DROP TABLE users";
+    $pdo->exec($sql);
+    print "table created successfully";
+  } 
+  catch (PDOException $e) 
+  {
+      //print "An Exception has occured " . $e->getMessage();
+      print "Something went wrong, please try again later.";
+  }
